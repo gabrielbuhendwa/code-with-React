a1c5468bde51199e735af68bd501de9c74d1da89
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-29 18:32:26 +0200

    add this to my repository

--- 2. Starting-project/src/App.jsx
+++ 2. Starting-project/src/App.jsx
@@ -1,5 +1,4 @@
 import reactImg from './assets/react-core-concepts.png';
-import ComponentsImg from './assets/config.png';
 import { CORE_CONCEPTS } from './data.JS'; 
 
 const reactDescriptions = ['Fundamental','Crucial','Core'];
@@ -47,21 +46,9 @@ function App() {
               description={CORE_CONCEPTS[0].description} 
               image={CORE_CONCEPTS[0].image} 
             />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title} 
-              description={CORE_CONCEPTS[1].description} 
-              image={CORE_CONCEPTS[1].image} 
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title} 
-              description={CORE_CONCEPTS[2].description} 
-              image={CORE_CONCEPTS[2].image}           
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title} 
-              description={CORE_CONCEPTS[3].description} 
-              image={CORE_CONCEPTS[3].image} 
-            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
