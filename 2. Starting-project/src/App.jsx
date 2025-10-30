f1a1d0f593062653e42a99ecf5f6c2f6027429bc
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-30 20:03:10 +0200

    add this to my repository

--- 2. Starting-project/src/App.jsx
+++ 2. Starting-project/src/App.jsx
@@ -35,13 +35,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>  
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>  
           </menu>
           { !selectedTopic ? ( 
-            <p>Please select a Topic</p>) : (<div id="tab-content">
+            <p>Please select a Topic</p>) : 
+          (<div id="tab-content">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
