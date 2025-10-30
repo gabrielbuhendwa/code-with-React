c64ce31bb8c6ca25427dee0438b7e95d368cad11
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-30 19:36:05 +0200

    add this to my repository

--- 2. Starting-project/src/App.jsx
+++ 2. Starting-project/src/App.jsx
@@ -4,10 +4,11 @@ import { CORE_CONCEPTS } from './data.JS';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.JS'; 
 
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Button Clicked');
+  const [selectedTopic, setSelectedTopic] = useState();
 
    function handleSelect(selectedButton){
         //selectedButton => 'components', 'jsx', 'props', 'state'
@@ -39,7 +40,17 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>  
           </menu>
-          {selectedTopic}
+          { !selectedTopic ? ( 
+            <p>Please select a Topic</p>) : (<div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre> 
+          </div>
+          )}
         </section>
       </main>
     </div>
