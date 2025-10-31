baed349a4a28435873e0766c84cee6df44d3feb2
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-31 11:18:35 +0200

    add this to my repository

--- 2. Starting-project/src/App.jsx
+++ 2. Starting-project/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header/>
       <main>
         <section id="core-concepts">
@@ -49,7 +49,7 @@ function App() {
           )}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
