063082d4e94988949c33e267185cf7ce76be8d83
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-31 11:53:58 +0200

    add this to my repository

--- 2. Starting-project/src/App.jsx
+++ 2. Starting-project/src/App.jsx
@@ -1,10 +1,10 @@
 import {useState} from 'react';
 
-import { CORE_CONCEPTS } from './data.JS'; 
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
 import { EXAMPLES } from './data.JS'; 
+import CoreConcept from './components/CoreConcept.jsx';
 
 
 function App() {
@@ -19,14 +19,7 @@ function App() {
     <>
       <Header/>
       <main>
-        <section id="core-concepts">
-          <h2>Core concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem}/>
-            ))}
-          </ul>
-        </section>
+        <CoreConcepts/>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
