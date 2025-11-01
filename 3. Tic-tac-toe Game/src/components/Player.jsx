959a33a82a68b4d2d67b40b73a739402716c95b8
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-11-01 03:37:35 +0200

    add this to my repository

--- 3. Tic-tac-toe Game/src/components/Player.jsx
+++ 3. Tic-tac-toe Game/src/components/Player.jsx
@@ -1,11 +1,26 @@
-export default function Player({ name, symbol, onEdit }) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [ isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick(){
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>
+
+  if (isEditing){
+    playerName = <input type="text" required value={name}/>;
+  }
+
   return (
+
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={onEdit}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
